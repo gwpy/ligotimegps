7a045398cf00ec03e1475400787387af8b01710a
Duncan Macleod <duncan.macleod@ligo.org>
2017-04-17 14:20:22 -0500

    setup.py: added python 2.6 classifier

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(name='ligotimegps',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
