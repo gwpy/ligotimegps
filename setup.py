28c9300bae907ed25c9197eeac8f040e5f0fa272
Duncan Macleod <duncan.macleod@ligo.org>
2021-05-07 11:29:38 +0100

    migrate metadata to setup.cfg
    - move build instructions to pyproject.toml
    - use setuptools-scm for automatic versioning

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) Duncan Macleod (2017)
+# Copyright (C) Cardiff University (2017-2021)
 #
 # This file is part of ligotimegps.
 #
@@ -20,59 +19,5 @@
 """Setup the ligotimegps package
 """
 
-import sys
 from setuptools import setup
-
-import versioneer
-
-__version__ = versioneer.get_version()
-
-# setup dependencies
-if set(('pytest', 'test', 'prt')).intersection(sys.argv):
-    setup_requires = ['pytest_runner']
-else:
-    setup_requires = []
-
-# runtime dependencies
-install_requires = []
-
-# test dependencies
-tests_require = ['pytest>=2.8']
-
-# read long description
-with open('README.md', 'rb') as f:
-    longdesc = f.read().decode().strip()
-
-# run setup
-setup(name='ligotimegps',
-      version=__version__,
-      packages=['ligotimegps'],
-      description="A pure-python version of lal.LIGOTimeGPS",
-      long_description=longdesc,
-      long_description_content_type='text/markdown',
-      author='Duncan Macleod',
-      author_email='duncan.macleod@ligo.org',
-      keywords="gravitational-waves ligo lal",
-      url="https://github.com/gwpy/ligotimegps",
-      license='GPLv3',
-      cmdclass=versioneer.get_cmdclass(),
-      python_requires='>=3.4',
-      setup_requires=setup_requires,
-      install_requires=install_requires,
-      tests_require=tests_require,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Intended Audience :: Science/Research',
-          'Natural Language :: English',
-          'Topic :: Scientific/Engineering',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Physics',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-      ],
-      )
+setup(use_scm_version=True)
