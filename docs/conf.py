3a723b55b49a9279bf278e71a60d12c37c172b3a
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-10-11 16:09:42 +0100

    Simplify sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,56 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# Copyright (c) 2017-2025 Cardiff University
 
+from ligotimegps import __version__ as LIGOTIMEGPS_VERSION
 
-from ligotimegps import __version__ as VERSION
-
-# -- Project information -----------------------------------------------------
+# -- Project information -------------
 
 project = "ligotimegps"
 copyright = "2010-2016, Kipp Cannon; 2017-2025 Cardiff University"
 author = "Duncan Macleod"
+release = LIGOTIMEGPS_VERSION
+version = "dev" if ".dev" in LIGOTIMEGPS_VERSION else LIGOTIMEGPS_VERSION
 
-# The full version, including alpha/beta/rc tags
-release = VERSION
+# -- General configuration -----------
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "myst_parser",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
-    "sphinx_automodapi.automodapi",
-    "sphinx_copybutton",
-    "sphinx_tabs.tabs",
-]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "monokai"
-
-# The reST default role (used for this markup: `text`) to use for all
-# documents.
+# Default role for single backticks
 default_role = "obj"
-
-# Default file type
-source_suffix = ".rst"
-
-# The master toctree document.
-master_doc = "index"
 
 # Epilogue
 rst_epilog = """
@@ -60,20 +23,21 @@ rst_epilog = """
 
 """
 
+# -- HTML formatting -----------------
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "furo"
+html_title = f"{project} {version}"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+# -- Extensions ----------------------
 
-# -- Extensions --------------------------------------------------------------
+extensions = [
+    "myst_parser",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx_automodapi.automodapi",
+    "sphinx_copybutton",
+    "sphinx_tabs.tabs",
+]
 
 # Intersphinx directory
 intersphinx_mapping = {
